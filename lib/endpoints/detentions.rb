21de8fb8e00dde4fd570bc7020a3766c1ec9108a
Ikraam Ghoor <ikraamghoor@nebulab.it>
2023-06-19 12:05:53 +0200

    Add beta endpoint to detentions

--- lib/endpoints/detentions.rb
+++ lib/endpoints/detentions.rb
@@ -3,6 +3,7 @@ module Wonde
     @@uri = 'detentions/'
     def initialize(token, id=false)
       super(token, id)
+      self.endpoint = 'https://api-beta.edge.wonde.com/v1.0/' # remove line when out of beta
       self.uri = @@uri
       self.uri = id + '/' + @@uri if id
       self.uri = self.uri.gsub("//", "/").chomp("/")
